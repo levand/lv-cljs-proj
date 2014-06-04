5a49174b94cfe849ca1a2eae388e2d442a80cba6
Luke VanderHart <luke@vanderhart.net>
2014-06-04 14:13:28 -0400

    release version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lv-cljs-proj/lein-template "0.1.0-SNAPSHOT"
+(defproject lv-cljs-proj/lein-template "0.1.0"
   :description "Cljx project template"
   :url "http://github.com/levand/lv-cljs-proj"
   :license {:name "Eclipse Public License"
